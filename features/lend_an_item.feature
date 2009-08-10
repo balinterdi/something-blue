d5944302cfc49a37572009df908d29f31b528449
Balint Erdi <balint.erdi@gmail.com>
2009-08-10 17:44:15 +0200

    lending an item feature passes

--- features/lend_an_item.feature
+++ features/lend_an_item.feature
@@ -1,15 +1,18 @@
+@current
 Feature: Lend an item
   In order to keep track of what I have lent
   As a user
   I want to register the fact of lending
   
   Scenario: User registers a lending
-  Given there is a user called "leonhard"
+  Given there is a user called "alice"
+  And there is a user called "bob"
+  And I am logged in as "alice"
   When I go to the new lending page
   And I fill in the lending's title with "My favorite worst nightmare"
   And I select "CD" from "category"
   And I select "2 weeks" from "time"
-  And I select "leonhard" from "to"
+  And I select "bob" from "to"
   And I press "Save"
   Then I should see "Lending saved"
   And I should see "My favorite worst nightmare"
--- features/step_definitions/setup_steps.rb
+++ features/step_definitions/setup_steps.rb
@@ -1,3 +1,23 @@
 Given /^there is a user called "([^\"]*)"$/ do |login|
   Factory(:user, :login => login)
 end
+
+# see the thread: http://www.ruby-forum.com/topic/182267#new
+Given /^I log in as "([^\"]*)"$/ do |login|
+  @user = User.by_login(login)
+  webrat.automate do
+    visit login_path
+    fill_in "user_session_login", :with => login
+    fill_in "user_session_password", :with => 'secret'
+    click_button 'Log in'
+  end
+
+  webrat.simulate do
+    post "/user_session", :user_session => { :login => @user.login, :password => 'secret' }
+  end
+end
+  
+Given /^I am logged in as "([^\"]*)"$/ do |login|
+  Given %(there is a user called "#{login}")
+  Given %(I log in as "#{login}")
+end
